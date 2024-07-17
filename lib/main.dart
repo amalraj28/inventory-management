294ff948b4f2db45dc0ad5d9e00779f47416dd62
Amal Raj <amalraj2701@gmail.com>
2024-07-17 13:10:32 +0530

    Added login screen provided by flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:inventory_management/firebase_options.dart';
-import 'package:inventory_management/screens/login_screen.dart';
+import 'package:inventory_management/screens/authenticate.dart';
 import 'package:inventory_management/screens/sell_item.dart';
 import 'package:inventory_management/screens/stock_entry.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env');
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -24,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const Authenticate(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -41,6 +45,29 @@ class MyHomePage extends StatelessWidget {
         title: const Text(
           'Inventory Management',
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<ProfileScreen>(
+                  builder: (context) => ProfileScreen(
+                    appBar: AppBar(
+                      title: const Text('User Profile'),
+                    ),
+                    actions: [
+                      SignedOutAction((context) {
+                        Navigator.of(context).pop();
+                      })
+                    ],
+                  ),
+                ),
+              );
+            },
+          )
+        ],
+        automaticallyImplyLeading: false,
       ),
       body: Center(
         child: Column(
@@ -78,19 +105,6 @@ class MyHomePage extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            ElevatedButton(
-              onPressed: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => LoginScreen(),
-                  ),
-                )
-              },
-              child: const Text(
-                'Login Screen',
-              ),
-            )
           ],
         ),
       ),
