991c0621e0f8b5f5eb03f424f8d2755baf73b872
Amal Raj <amalraj2701@gmail.com>
2024-07-24 17:17:04 +0530

    Handled error_codes while signing in or signing up

--- lib/auth/auth_services.dart
+++ lib/auth/auth_services.dart
@@ -1,36 +1,88 @@
 import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
+
+class Result<T> {
+  final T? data;
+  final String? error;
+
+  Result({this.data, this.error});
+
+  bool get isSuccess => data != null;
+  bool get isError => error != null;
+}
 
 class AuthServices {
   final _auth = FirebaseAuth.instance;
 
-  Future<User?> signUpWithEmail(String email, String password) async {
+  Future<Result<User?>> signUpWithEmail(String email, String password) async {
     try {
       final credentials = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
-      return credentials.user;
-    } catch (e) {
-      log('Something went wrong');
-    }
+      return Result(data: credentials.user);
+    } on FirebaseAuthException catch (e) {
+      late final String message;
+      switch (e.code) {
+        case 'email-already-in-use':
+          message = 'Email is already registered';
+          break;
 
-    return null;
+        case 'invalid-email':
+          message = 'Please enter a valid email';
+          break;
+
+        case 'operation-not-allowed':
+          message = 'Sorry, this account is not authorised to access this app.';
+          break;
+
+        case 'weak-password':
+          message = 'Please create a strong password';
+          break;
+
+        default:
+          message = 'Unknown error. Please try again later!';
+          break;
+      }
+
+      return Result(error: message);
+    }
   }
 
-  Future<User?> signInWithEmail(String email, String password) async {
+  Future<Result<User?>> signInWithEmail(String email, String password) async {
     try {
       final credentials = await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      return credentials.user;
-    } catch (e) {
-      log('Something went wrong');
-    }
 
-    return null;
+      return Result(data: credentials.user);
+    } on FirebaseAuthException catch (e) {
+      late final String message;
+      switch (e.code) {
+        case 'email-already-in-use':
+          message = 'Email is already registered';
+          break;
+
+        case 'invalid-email':
+          message = 'Please enter a valid email';
+          break;
+
+        case 'operation-not-allowed':
+          message = 'Sorry, this account is not authorised to access this app.';
+          break;
+
+        case 'weak-password':
+          message = 'Please create a strong password';
+          break;
+
+        default:
+          message = 'Unknown error. Please try again later!';
+          break;
+      }
+
+      return Result(error: message);
+    }
   }
 
   Future<void> signOut() async {
